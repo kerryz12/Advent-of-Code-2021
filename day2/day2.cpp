d8cefe723b9f279cb8d27ddea5bf6cf048e2c5ef
ker1200 <60584013+ker1200@users.noreply.github.com>
2021-12-02 12:44:14 -0800

    day 2

---
+++ day2/day2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+vector<string> input;
+string line;
+ifstream file("day2.txt");
+
+int part1();
+int part2();
+
+int main (void) {
+
+    while(getline(file, line)) {
+        input.push_back(line);
+    }
+
+    cout << part1() << "\n";
+    cout << part2();
+
+    return 0;
+}
+
+int part1() {
+    int x = 0;
+    int y = 0;
+
+    for (string s : input) {
+        if(s[0] == 'f') {
+            x += (s[8] - '0');
+        }
+        if (s[0] == 'd') {
+            y += (s[5] - '0');
+        }
+        if(s[0] == 'u') {
+            y -= (s[3] - '0');
+        }
+    }
+
+    return x * y;
+}
+
+int part2() {
+    int x = 0;
+    int y = 0;
+    int aim = 0;
+
+    for (string s : input) {
+        if(s[0] == 'f') {
+            x += (s[8] - '0');
+            y += aim * (s[8] - '0');
+        }
+        if (s[0] == 'd') {
+            aim += (s[5] - '0');
+        }
+        if(s[0] == 'u') {
+            aim -= (s[3] - '0');
+        }
+    }
+
+    return x * y;
+}   
